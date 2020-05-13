03acd3338e53741c65a2cf0e1af108d25e954040
Matthew Oslan <derpthemeus@gmail.com>
2020-05-13 09:47:21 -0400

    Update default OTF cloud server configuration

--- OpenGarage/OpenGarage.cpp
+++ OpenGarage/OpenGarage.cpp
@@ -64,8 +64,8 @@ OptionStruct OpenGarage::options[] = {
   {"ssid", 0, 0, ""},  // string options have 0 max value
   {"pass", 0, 0, ""},
   {"auth", 0, 0, ""},
-  {"bdmn", 0, 0, "cloud.openthings.io"},
-  {"bprt", 8080,65535, ""},
+  {"bdmn", 0, 0, "ws.test.openthings.io"},
+  {"bprt", 80,65535, ""},
   {"dkey", 0, 0, DEFAULT_DKEY},
   {"name", 0, 0, DEFAULT_NAME},
   {"iftt", 0, 0, ""},
